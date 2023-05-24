8a11018093ae9332426b889be954b8151024322a
Agostina Contreras <agoscontreras1@gmail.com>
2023-05-24 20:31:47 -0300

    ejercicios finalizados

---
+++ javascript.js
@@ -0,0 +1,57 @@
+ //punto 1
+var numerosSuma = 4 + 6;
+console.log(numerosSuma);
+
+
+//punto 2
+var num1 = 100;
+var num2 = 50;
+
+let sumaNumeros = num1 + num2;
+console.log(sumaNumeros);
+
+
+//punto 3 
+var numero =  prompt ("ingrese su numero");
+
+if(numero % 2 == 0){
+   console.log(numero + " par");
+}else{
+   console.log(numero + " impar");
+}
+
+
+
+
+//punto 4
+var nombre= "Agostina";
+console.log(nombre.length);
+
+
+
+//punto 6 
+for(i=1; i<=100; i++){
+console.log(i);
+}
+
+
+//punto 7
+var arrayNumeros= [
+    [2, 4, 6], // indice 0
+    [3, 5, 7], // indice 1
+
+ ]
+ for(var i =0; i<arrayNumeros.length; i++){
+    console.log(arrayNumeros[i]);
+ }
+
+//punto 8
+
+var nombre= "Hola mundo";
+console.log(nombre.length);
+
+
+//punto 10
+
+var array =  [1, 2, 3, 4, 5, 6];
+
